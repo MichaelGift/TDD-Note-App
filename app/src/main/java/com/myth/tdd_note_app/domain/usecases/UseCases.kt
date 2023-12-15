af045210bf6ed622a9408c1637327f3b3316cdf3
MichaelGift <simiyugift@outlook.com>
2023-12-16 02:33:51 +0300

    Create data class for all usecases

---
+++ app/src/main/java/com/myth/tdd_note_app/domain/usecases/UseCases.kt
@@ -0,0 +1,8 @@
+package com.myth.tdd_note_app.domain.usecases
+
+data class UseCases(
+    val addNote: AddNotesUseCase,
+    val deleteNote : DeleteNoteUseCase,
+    val getAllNotes: GetNotesUseCase,
+    val getNoteById: GetNoteByIdUseCase
+)
