b9b928ec8054e982daa99015a63358893b76498e
MichaelGift <simiyugift@outlook.com>
2023-12-16 19:10:48 +0300

    Add events for SaveNote

---
+++ app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/events/UiEvent.kt
@@ -0,0 +1,6 @@
+package com.myth.tdd_note_app.presentation.add_edit_note.events
+
+sealed class UiEvent {
+    data class ShowSnackBar(val message: String) : UiEvent()
+    object SaveNote : UiEvent()
+}
