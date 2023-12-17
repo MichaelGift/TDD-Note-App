13f370b1a22f76348e71bd72a6d6173a19572770
MichaelGift <simiyugift@outlook.com>
2023-12-17 22:38:25 +0300

    Refactor: Remove manual hint updates

--- app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/states/TextFieldState.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/states/TextFieldState.kt
@@ -2,6 +2,4 @@ package com.myth.tdd_note_app.presentation.add_edit_note.states
 
 data class TextFieldState(
     val text: String = "",
-    val hint: String = "",
-    val showHint: Boolean = false
 )
