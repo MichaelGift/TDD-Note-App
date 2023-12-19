296db1e17405ed1a29757d5897f694b5b000228e
MichaelGift <simiyugift@outlook.com>
2023-12-20 01:01:07 +0300

    Create composable for Note

---
+++ app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/components/NoteItem.kt
@@ -0,0 +1,96 @@
+package com.myth.tdd_note_app.presentation.notes_list.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.myth.tdd_note_app.domain.model.Note
+import com.myth.tdd_note_app.ui.theme.TDDNoteAppTheme
+
+@Composable
+fun NoteItem(
+    note: Note,
+    onDeleteClick: () -> Unit
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(8.dp),
+        modifier = Modifier.padding(all = 2.dp),
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .width(8.dp)
+                    .background(Color.Red)
+                    .fillMaxHeight()
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = note.title,
+                        style = MaterialTheme.typography.headlineSmall,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = note.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNoteItem() {
+    val note = Note(
+        1,
+        "Sample Title",
+        "Sample Content Sample Content Sample Content Sample Content Sample Content Sample Content"
+    )
+    val notes = listOf(note, note, note, note, note, note, note, note, note, note, note)
+    TDDNoteAppTheme {
+
+        Surface {
+            LazyColumn {
+                items(notes) { note ->
+                    NoteItem(note = note) {}
+                }
+            }
+        }
+    }
+}
