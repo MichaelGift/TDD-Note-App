1ead0e4e386a50e57e7e2eb9cad1aff245ca53a6
MichaelGift <simiyugift@outlook.com>
2023-12-19 13:56:45 +0300

    Refactor: change to data object

--- app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/events/AddEditEvent.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/events/AddEditEvent.kt
@@ -1,9 +1,7 @@
 package com.myth.tdd_note_app.presentation.add_edit_note.events
 
-import androidx.compose.ui.focus.FocusState
-
 sealed class AddEditEvent {
     data class EditNoteTitle(val noteTitle: String) : AddEditEvent()
     data class EditNoteContent(val noteContent: String) : AddEditEvent()
-    object SaveNote : AddEditEvent()
+    data object SaveNote : AddEditEvent()
 }
