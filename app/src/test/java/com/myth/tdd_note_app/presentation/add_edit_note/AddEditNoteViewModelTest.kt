f8b5791c279407ea75c4c327617f7475ef6e1e16
MichaelGift <simiyugift@outlook.com>
2023-12-19 13:58:35 +0300

    Refactor: Rewrite tests for ViewModel

--- app/src/test/java/com/myth/tdd_note_app/presentation/add_edit_note/AddEditNoteViewModelTest.kt
+++ app/src/test/java/com/myth/tdd_note_app/presentation/add_edit_note/AddEditNoteViewModelTest.kt
@@ -1,15 +1,16 @@
 package com.myth.tdd_note_app.presentation.add_edit_note
 
-import androidx.compose.ui.focus.FocusState
 import androidx.lifecycle.SavedStateHandle
+import com.google.common.truth.Truth.assertThat
 import com.myth.tdd_note_app.MainDispatcherRule
+import com.myth.tdd_note_app.data.repository.FakeRepository
+import com.myth.tdd_note_app.domain.model.Note
+import com.myth.tdd_note_app.domain.usecases.AddNotesUseCase
+import com.myth.tdd_note_app.domain.usecases.GetNoteByIdUseCase
 import com.myth.tdd_note_app.domain.usecases.UseCases
 import com.myth.tdd_note_app.presentation.add_edit_note.events.AddEditEvent
-import com.myth.tdd_note_app.presentation.add_edit_note.events.UiEvent
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -18,36 +19,93 @@ import org.mockito.Mockito.mock
 class AddEditNoteViewModelTest {
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
-    private val savedStateHandle = SavedStateHandle(mapOf("noteId" to 1))
-    private lateinit var useCase: UseCases
+    private lateinit var savedStateHandle: SavedStateHandle
+
+    private lateinit var repository: FakeRepository
+    private lateinit var saveNote: AddNotesUseCase
+    private lateinit var searchNoteId: GetNoteByIdUseCase
+    private lateinit var useCases: UseCases
     private lateinit var viewModel: AddEditNoteViewModel
+
+    private val note1 = Note(1, "Title1", "Content1")
+    private val note2 = Note(2, "Title2", "Content2")
+    private val samples = listOf(note1, note2)
+
     @Before
-    fun setUp() {
-        useCase = UseCases(mock(), mock(), mock(), mock())
-        viewModel = AddEditNoteViewModel(useCase, savedStateHandle)
+    fun setup() {
+        repository = FakeRepository()
+        saveNote = AddNotesUseCase(repository)
+        searchNoteId = GetNoteByIdUseCase(repository)
+        useCases =
+            UseCases(saveNote, mock(), mock(), searchNoteId)
+        repository.addSampleNotes(samples)
     }
 
     @Test
-    fun viewModelInitialization_withNoteId_loadsNote() = runTest {
-        assertEquals(1, viewModel.currentNoteId)
+    fun `when editing a note currentNodeId is updated on init`() {
+        val id = 1
+        savedStateHandle = SavedStateHandle(mapOf("noteId" to id))
+        viewModel = AddEditNoteViewModel(useCases, savedStateHandle)
+        assertThat(viewModel.currentNoteId).isEqualTo(id)
     }
 
     @Test
-    fun onEvent_editNoteTitle_editsNoteTitles() {
-        viewModel.onEvent(AddEditEvent.EditNoteTitle("New Title"))
-        assertEquals("New Title", viewModel.noteTitle.value.text)
+    fun `when editing a note noteContent and noteTitle are updated correctly`() = runTest {
+        val id = 1
+        viewModel = AddEditNoteViewModel(useCases, SavedStateHandle(mapOf("noteId" to id)))
+
+        val note = repository.getNoteById(id)
+        val noteTitle = viewModel.noteTitle.value.text
+        val noteContent = viewModel.noteContent.value.text
+
+        assertThat(noteTitle).isEqualTo(note?.title)
+        assertThat(noteContent).isEqualTo(note?.content)
     }
 
     @Test
-    fun onEvent_editNoteContent_editsNoteContent(){
-        viewModel.onEvent(AddEditEvent.EditNoteContent("New Content"))
-        assertEquals("New Content", viewModel.noteContent.value.text)
+    fun `noteTitle updates on EditNoteTitle event`() {
+        viewModel = AddEditNoteViewModel(useCases, SavedStateHandle())
+
+        val newTitle = "Updated Note Title"
+        viewModel.onEvent(AddEditEvent.EditNoteTitle(newTitle))
+
+        val noteTitle = viewModel.noteTitle.value.text
+
+        assertThat(noteTitle).isEqualTo(newTitle)
+    }
+
+
+    @Test
+    fun `noteContent updates on EditNoteContent event`() {
+        viewModel = AddEditNoteViewModel(useCases, SavedStateHandle())
+
+        val newContent = "Updated Note Content"
+        viewModel.onEvent(AddEditEvent.EditNoteContent(newContent))
+
+        val noteContent = viewModel.noteContent.value.text
+
+        assertThat(noteContent).isEqualTo(newContent)
     }
 
     @Test
-    fun onEvent_saveNote_triggersSaveNoteEvent() = runTest {
+    fun `update note on SaveNote event if currentNoteId is not null`() = runTest {
+        val id = 1
+        viewModel = AddEditNoteViewModel(useCases, SavedStateHandle(mapOf("noteId" to id)))
+
+        val newTitle = "Updated Note Title"
+        viewModel.onEvent(AddEditEvent.EditNoteTitle(newTitle))
+
+        val newContent = "Updated Note Content"
+        viewModel.onEvent(AddEditEvent.EditNoteContent(newContent))
+
         viewModel.onEvent(AddEditEvent.SaveNote)
-        val event =  viewModel.eventFlow.first()
-        assertTrue(event is  UiEvent.SaveNote)
+
+        val currentNoteId = viewModel.currentNoteId!!
+        val newNote = Note(currentNoteId, newTitle, newContent)
+        val notes = repository.getAllNotes().first()
+
+        assertThat(notes).contains(newNote)
     }
+
+
 }
