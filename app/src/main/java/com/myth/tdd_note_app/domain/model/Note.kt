1e1c725d357be0f4fb51875b5057be1e628ccb35
MichaelGift <simiyugift@outlook.com>
2023-12-19 13:56:15 +0300

    Refactor: Change id to var
    For easier assignment

--- app/src/main/java/com/myth/tdd_note_app/domain/model/Note.kt
+++ app/src/main/java/com/myth/tdd_note_app/domain/model/Note.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes")
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @PrimaryKey(autoGenerate = true) var id: Int? = null,
     var title: String,
     var content: String
 )
