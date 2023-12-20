31126a345bb4852a816e53660317dd9d26b70350
MichaelGift <simiyugift@outlook.com>
2023-12-20 05:17:24 +0300

    Include message to Delete event

--- app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/events/UiEvent.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/add_edit_note/events/UiEvent.kt
@@ -2,6 +2,6 @@ package com.myth.tdd_note_app.presentation.add_edit_note.events
 
 sealed class UiEvent {
     data class ShowSnackBar(val message: String) : UiEvent()
-    object SaveNote : UiEvent()
-    object DeleteNote : UiEvent()
+    data object SaveNote : UiEvent()
+    data class DeleteNote(val message: String) : UiEvent()
 }
