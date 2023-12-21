bd905e3e7dbfcf20496dc96f49a5e8a09886e794
MichaelGift <simiyugift@outlook.com>
2023-12-21 21:24:54 +0300

    Add Toggle Search bar event

--- app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/events/NoteEvent.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/events/NoteEvent.kt
@@ -3,6 +3,8 @@ package com.myth.tdd_note_app.presentation.notes_list.events
 import com.myth.tdd_note_app.domain.model.Note
 
 sealed class NoteEvent {
-    data class DeleteNote(val note: Note): NoteEvent()
+    data class DeleteNote(val note: Note) : NoteEvent()
+    data class SearchNote(val query: String) : NoteEvent()
     data object RestoreNote : NoteEvent()
+    data object ToggleSearchBar : NoteEvent()
 }
