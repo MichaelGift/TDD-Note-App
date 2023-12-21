9128b8b7caace39d95d8b4c1b22db62b11fe0cb9
MichaelGift <simiyugift@outlook.com>
2023-12-21 21:25:54 +0300

    Add searching boolean to NotesState

--- app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/states/NotesState.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/states/NotesState.kt
@@ -2,4 +2,7 @@ package com.myth.tdd_note_app.presentation.notes_list.states
 
 import com.myth.tdd_note_app.domain.model.Note
 
-data class NotesState(val notes: List<Note> = emptyList())
+data class NotesState(
+    val notes: List<Note> = emptyList(),
+    var isSearching: Boolean = false,
+)
