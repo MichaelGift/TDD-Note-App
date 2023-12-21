9fd14d7939f841ed175bf3edbb36d93acb36fe47
MichaelGift <simiyugift@outlook.com>
2023-12-21 21:24:33 +0300

    Update composables

--- app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/components/NoteItem.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/components/NoteItem.kt
@@ -9,22 +9,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
@@ -37,55 +31,44 @@ import com.myth.tdd_note_app.ui.theme.TDDNoteAppTheme
 fun NoteItem(
     note: Note,
     modifier: Modifier = Modifier,
-    onDeleteClick: () -> Unit
 ) {
-    Box(modifier = modifier) {
-        Card(
-            elevation = CardDefaults.cardElevation(8.dp),
-            modifier = Modifier.padding(all = 4.dp),
-            shape = RoundedCornerShape(8.dp),
-        ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Box(
-                    modifier = Modifier
-                        .width(8.dp)
-                        .background(Color.Red)
-                        .fillMaxHeight()
-                )
+    ElevatedCard(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(8.dp),
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .width(8.dp)
+                    .background(Color.Red)
+                    .fillMaxHeight()
+            )
 
-                Spacer(modifier = Modifier.width(4.dp))
+            Spacer(modifier = Modifier.width(4.dp))
 
-                Column {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = note.title,
-                            style = MaterialTheme.typography.titleMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(8.dp))
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                     Text(
-                        text = note.content,
-                        style = MaterialTheme.typography.bodyMedium,
-                        maxLines = 8,
+                        text = note.title,
+                        style = MaterialTheme.typography.titleMedium,
+                        maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
                 }
+
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = note.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(4.dp))
             }
-        }
-        IconButton(
-            onClick = onDeleteClick,
-            modifier = Modifier.align(Alignment.BottomEnd)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Delete,
-                contentDescription = "Delete Note"
-            )
         }
     }
 }
@@ -104,7 +87,7 @@ fun PreviewNoteItem() {
         Surface {
             LazyColumn {
                 items(notes) { note ->
-                    NoteItem(note = note, Modifier) {}
+                    NoteItem(note = note, Modifier)
                 }
             }
         }
