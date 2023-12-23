3f99c5a8bb17b1b72b326a3f5b9211a0ddda2328
MichaelGift <simiyugift@outlook.com>
2023-12-24 02:50:50 +0300

    Move functions into curley brackets

--- app/src/main/java/com/myth/tdd_note_app/domain/usecases/DeleteNoteUseCase.kt
+++ app/src/main/java/com/myth/tdd_note_app/domain/usecases/DeleteNoteUseCase.kt
@@ -4,5 +4,7 @@ import com.myth.tdd_note_app.domain.model.Note
 import com.myth.tdd_note_app.domain.repository.NoteRepository
 
 class DeleteNoteUseCase(private val repository: NoteRepository) {
-    suspend operator fun invoke(note: Note) = repository.deleteNote(note)
+    suspend operator fun invoke(note: Note){
+        repository.deleteNote(note)
+    }
 }
