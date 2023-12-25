987a089f6c6870ddcb0619aca161b69594b47354
MichaelGift <simiyugift@outlook.com>
2023-12-25 04:09:39 +0300

    Remove search query

--- app/src/main/java/com/myth/tdd_note_app/data/data_source/NoteDao.kt
+++ app/src/main/java/com/myth/tdd_note_app/data/data_source/NoteDao.kt
@@ -21,7 +21,4 @@ interface NoteDao {
 
     @Delete
     suspend fun deleteNote(note: Note)
-
-    @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%'")
-    fun searchNote(query: String): Flow<List<Note>>
 }
