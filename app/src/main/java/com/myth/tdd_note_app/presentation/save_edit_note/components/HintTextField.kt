418c7bf779725c8c06227aee29dc81251aa8ea5e
MichaelGift <simiyugift@outlook.com>
2023-12-25 04:28:25 +0300

    Readd Hint Text Field

---
+++ app/src/main/java/com/myth/tdd_note_app/presentation/save_edit_note/components/HintTextField.kt
@@ -0,0 +1,24 @@
+package com.myth.tdd_note_app.presentation.save_edit_note.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HintTextField(
+    text: String,
+    label: String,
+    modifier: Modifier = Modifier,
+    singleLine: Boolean = false,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        value = text,
+        singleLine = singleLine,
+        onValueChange = onValueChange,
+        modifier = modifier.fillMaxWidth(),
+        label = { Text(text = label) },
+    )
+}
