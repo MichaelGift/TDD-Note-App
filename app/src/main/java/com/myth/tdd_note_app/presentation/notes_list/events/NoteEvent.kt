dfabdeea57ca8c5e2bd40fd23d966bb450de07a0
MichaelGift <simiyugift@outlook.com>
2023-12-25 04:12:51 +0300

    Update note event to include search query

--- app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/events/NoteEvent.kt
+++ app/src/main/java/com/myth/tdd_note_app/presentation/notes_list/events/NoteEvent.kt
@@ -1,10 +1,11 @@
 package com.myth.tdd_note_app.presentation.notes_list.events
 
 import com.myth.tdd_note_app.domain.model.Note
+import com.myth.tdd_note_app.presentation.notes_list.states.SearchWidgetState
 
 sealed class NoteEvent {
     data class DeleteNote(val note: Note) : NoteEvent()
     data class SearchNote(val query: String) : NoteEvent()
     data object RestoreNote : NoteEvent()
-    data object ToggleSearchBar : NoteEvent()
+    data class ToggleSearchBar(val value: SearchWidgetState) : NoteEvent()
 }
